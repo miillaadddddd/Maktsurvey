ae0aa37ed2b78b010efd2fa2e18e31dbd6bb7180
miillaadddddd <milad7212@gmail.com>
2022-02-01 22:24:31 +0330

    Add llottiFile

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import lottie from "lottie-web";
 
 function App() {
+  let animation1=null;
   const [controlerPage, setControlerPage] = useState(1);
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: animation1.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: require("../src/images/manifest.json"),
+
+      //   path: "./lottie/animation.json",
+    });
+  }, []);
   return (
     <div className="h-screen bg-slate-700 flex flex-col justify-center items-center">
-      <div className="w-1/3 h-12 bg-gray-200 mb-3 rounded-xl flex justify-center"></div>
-      <div className="w-2/3 bg-gray-200 h-2/3 rounded-xl shadow-lg relative">
-        <div className="absolute top-[50%] left-2  p-2 text-xl cursor-pointer duration-300 hover:scale-125">
+      
+      <div className="w-24 h-12 bg-gray-200 mb-3 rounded-xl flex justify-center items-center relative">
+        2:00
+        <div className="count-page  left-[-60px]">5</div>
+        <div className="count-page  right-[-60px]">{controlerPage}</div>
+      </div>
+      <div  className="w-2/3 bg-gray-200 h-2/3 rounded-xl shadow-lg relative">
+        <div onClick={()=>{
+          if(controlerPage !== 1){
+            setControlerPage(controlerPage-1)
+          }
+        }} className="icon right-2 ">
           &#10094;
         </div>
-        <div className="absolute top-[50%] right-2  p-2  text-xl cursor-pointer duration-300 hover:scale-125">
+        <div onClick={()=>{
+          if(controlerPage !== 5){
+            setControlerPage(controlerPage+1)
+          }
+        }} className="icon  left-2 ">
           &#10095;
         </div>
-        <div className=""></div>
+        <div ref={animation1} className=""></div>
       </div>
     </div>
   );
