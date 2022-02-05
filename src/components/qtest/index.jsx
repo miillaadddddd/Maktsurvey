1bc8e1a9dd6d16717c0d087fecbbafd5b97cdccb
miillaadddddd <milad7212@gmail.com>
2022-02-05 13:59:51 +0330

    Feat : check box

--- src/components/qtest/index.jsx
+++ src/components/qtest/index.jsx
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
 
 function Qtest() {
-  const [qtest, setQtest] = useState("");
+  const [qtest, setQtest] = useState({
+    isgood: true,
+    isbad: false,
+  });
+
+  const selectCheck = () => {
+    let newAnswre = {};
+    for (let [key, value] of Object.entries(qtest)) {
+      newAnswre[key] = !value;
+    }
+    setQtest(newAnswre);
+  };
   return (
     <>
       <div className="w-[80%]">
@@ -10,19 +21,32 @@ function Qtest() {
             class="block text-gray-700 text-sm font-bold mb-4"
             for="username"
           >
-            نظرتون درباره برنامه نویسی من چیه؟ :
+            نظرتون درباره برنامه نویسی من چیه؟
           </label>
-          <textarea
-            class="shadow  appearance-none border rounded-xl w-full py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="username"
-            value={qtest}
-            onChange={(e) => {
-              localStorage.setItem("#qone", e.target.value);
-              setQtest(e.target.value);
-            }}
-            type="text"
-            placeholder="من کرمانی هستم که آقا محمد خان قاجار بهش حمله کرده وچشمان تمامی مردمانم کور شده"
-          />
+
+          <div className="">
+            <input
+              name="isgood"
+              type="checkbox"
+              checked={qtest.isgood}
+              onChange={() => {
+                selectCheck();
+              }}
+            />
+            <label>خوبه</label>
+          </div>
+
+          <div className="">
+            <input
+              name="isbad"
+              type="checkbox"
+              checked={qtest.isbad}
+              onChange={() => {
+                selectCheck();
+              }}
+            />
+            <label>بده</label>
+          </div>
         </form>
       </div>
     </>
